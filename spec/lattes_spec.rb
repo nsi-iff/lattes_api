b8380bf1447c8c7d2f20d18a4f47aba89d35583b
Rodrigo Manhães <rmanhaes@gmail.com>
2014-09-02 01:26:05 -0300

    refactor access denied (closes #3)

--- spec/lattes_spec.rb
+++ spec/lattes_spec.rb
@@ -7,6 +7,8 @@ describe LattesApi do
   let(:id_cnpq) { '1234567890123456' }
   let(:cpf) { '123.456.789-09' }
 
+  let(:client) { LattesApi::Client.new }
+
   it 'check version' do
     expect(LattesApi::VERSION).to eq '0.0.1'
   end
@@ -19,7 +21,6 @@ describe LattesApi do
     it 'happy path' do
       savon.expects(:get_identificador_cn_pq).with(message: message).returns(
         response(:get_identificador_cnpq, id_cnpq))
-      client = LattesApi::Client.new
       expect(client.get_identificador_cnpq(cpf)).to eq id_cnpq
     end
 
@@ -27,7 +28,6 @@ describe LattesApi do
       savon.expects(:get_identificador_cn_pq).
         with(message: message).
         returns(access_denied('getIdentificadorCNPq'))
-      client = LattesApi::Client.new
       expect {
         client.get_identificador_cnpq(cpf)
       }.to raise_error LattesApi::AccessDenied
@@ -41,7 +41,6 @@ describe LattesApi do
       result = encodeFile(id_cnpq)
       savon.expects(:get_curriculo_compactado).with(message: message).returns(
         response(:get_curriculo_compactado, result))
-      client = LattesApi::Client.new
       expect(client.get_curriculo_compactado(id_cnpq)).to eq result
     end
 
@@ -49,7 +48,6 @@ describe LattesApi do
       savon.expects(:get_curriculo_compactado).
         with(message: message).
         returns(access_denied('getCurriculoCompactado'))
-      client = LattesApi::Client.new
       expect {
         client.get_curriculo_compactado(id_cnpq)
       }.to raise_error LattesApi::AccessDenied
